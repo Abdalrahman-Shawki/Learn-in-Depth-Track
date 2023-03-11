512056b61a416034b9099f8da2e57f71c7b40ce3
Abdalrahman-Shawki <Abdalrahman.Shawki93@Gmail.Com>
2023-03-11 16:20:39 +0200

    Edit the folders name inside 0x00-First_Term_Track/0x00-C_Programming_Lang_and_Git/

--- 0x00-First_Term_Track/0x00-C_Programming_Lang_and_Git/0x01-C_Basic Assignment/Homework 01/EX 02/main.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(void)
-{
-    int Enter_Num = 0;
-    printf("Enter an integer: ");
-    scanf("%d", & Enter_Num);
-    printf("You Entered: %d",Enter_Num);
-}
--- 0x00-First_Term_Track/0x00-C_Programming_Lang_and_Git/0x01-C_Basic Assignment/Homework 01/EX 05/main.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(void)
-{
-    char Character = 0;
-    printf("Enter a Character: ");
-    scanf("%c", & Character);
-    printf("ASCII value of %c = %d", Character, Character);
-}
